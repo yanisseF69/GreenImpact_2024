38f64c34d1ef7fd8048b09a7943973d1b48d1071
Yanisse <yanisseferhaoui@gmail.com>
2024-04-05 14:45:05 +0200

    start questionnaire ok

--- backend/greenimpact/models.py
+++ backend/greenimpact/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Categorie(models.Model):
+
+    nom_categorie = models.CharField(max_length=20, unique=True)
+
+    def __str__(self) -> str:
+        return self.nom_categorie
+
+class Question(models.Model):
+
+    titre = models.CharField(max_length=100)
+
+    categorie = models.ForeignKey(Categorie, related_name="question", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.titre
+
+class Option(models.Model):
+
+    question = models.ForeignKey(Question, related_name="reponse", on_delete=models.CASCADE)
+
+    texte_option = models.CharField(max_length=25)
+    
+    valeur_carbone = models.FloatField()
+
+
--- backend/greenimpact/views.py
+++ backend/greenimpact/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.db import connection
+import random
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello World !")
+def start(request):
+
+    ids = []
+    questions=[]
+
+    while (len(ids) < 8):
+
+        with connection.cursor() as cursor:
+            categorie_id = random.randint(1,4)
+            query = "SELECT titre, categorie_id, id FROM greenimpact_question WHERE categorie_id = %s ORDER BY RANDOM() LIMIT 1"
+            
+
+            cursor.execute(query, [categorie_id])
+            row = cursor.fetchone()
+            print(row[2])
+            if row[2] not in ids:
+                print(len(ids))
+                ids.append(row[2])
+                query = "SELECT nom_categorie FROM greenimpact_categorie WHERE id = %s"
+                cursor.execute(query, [row[1]])
+                categories = cursor.fetchall()
+                query = "SELECT texte_option, valeur_carbone FROM greenimpact_option WHERE id IN (SELECT option_id FROM greenimpact_question_option WHERE question_id = %s);"
+                cursor.execute(query, [row[2]])
+                options = cursor.fetchall()
+                noms = [option[0] for option in options]
+                valeurs = [option[1] for option in options]
+                question = {
+                    "titre" : row[0],
+                    "categorie" : categories[0][0],
+                    "options" : {
+                        "noms" : noms,
+                        "valeurs_carbonne" : valeurs,
+                    }
+                }
+                questions.append(question)
+
+
+    return JsonResponse(questions, safe=False)
