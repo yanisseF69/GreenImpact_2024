9628770c0af9bea26e1c06c3831268bcdd76fcc0
Amira Rabehi <amira.rabehi@etu.univ-lyon1.fr>
2024-04-26 10:28:17 +0200

    separate logic into more functions

--- backend/greenimpact/views.py
+++ backend/greenimpact/views.py
@@ -8,77 +8,70 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 
-def start(request):
-    '''
-    This function is used to get the first 8 questions to display on the start page.
-    '''
-
-    page_number = request.GET.get('page', 1)  # Get the page number from the request
-    questions_per_page = 1  
-    total_questions_to_display = 10  
-
+def get_question_info(total_questions_to_display):
     with connection.cursor() as cursor:
-        # Fetch the first 10 question IDs, types, and categories
         cursor.execute(
             '''SELECT id_typage, nom_typage, id_categ
                FROM greenimpact_typage
                ORDER BY id_typage ASC
                LIMIT %s''', [total_questions_to_display]
         )
-        question_info = cursor.fetchall()
+        return cursor.fetchall()
 
-    # Prepare questions for pagination
+def get_question_details(id_typage):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            '''SELECT texte_question, type_question
+               FROM greenimpact_question 
+               WHERE id_typ = %s''', [id_typage]
+        )
+        return cursor.fetchone()
+
+def get_category_name(id_categ):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            '''SELECT nom_categorie
+               FROM greenimpact_categorie 
+               WHERE id_categorie = %s''', [id_categ]
+        )
+        return cursor.fetchone()
+
+def get_options(id_typage):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            '''SELECT texte_option, empreinte_carbonne, id_option
+               FROM greenimpact_option
+               WHERE id_typ = %s LIMIT 5;''', [id_typage]
+        )
+        return cursor.fetchall()
+
+def prepare_questions(question_info):
+    questions = []
+    for id_typage, nom_typage, id_categ in question_info:
+        quest = get_question_details(id_typage)
+        categorie = get_category_name(id_categ)
+        options = get_options(id_typage)
+        choix = [{"id": option[2], "nom": option[0], "valeur": option[1]} for option in options]
+        questions.append({
+            "titre": quest[0],
+            "categorie": categorie[0],
+            "type": nom_typage,
+            "choix": choix,
+            "unique": quest[1] == 1,
+        })
+    return questions
+
+def start(request):
+    page_number = request.GET.get('page', 1)
+    questions_per_page = 1
+    total_questions_to_display = 10
+
+    question_info = get_question_info(total_questions_to_display)
     paginator = Paginator(question_info, questions_per_page)
     page_obj = paginator.get_page(page_number)
+    page_questions = prepare_questions(page_obj.object_list)
 
-    # Prepare questions for the current page
-    page_questions = []
-    for id_typage, nom_typage, id_categ in page_obj:
-        with connection.cursor() as cursor:
-            # Fetch the question details
-            cursor.execute(
-                '''SELECT texte_question, type_question
-                   FROM greenimpact_question 
-                   WHERE id_typ = %s''', [id_typage]
-            )
-            quest = cursor.fetchone()
-
-            # Fetch the category name
-            cursor.execute(
-                '''SELECT nom_categorie
-                   FROM greenimpact_categorie 
-                   WHERE id_categorie = %s''', [id_categ]
-            )
-            categorie = cursor.fetchone()
-
-            # Fetch the options for the question
-            cursor.execute(
-                '''SELECT texte_option, empreinte_carbonne, id_option
-                   FROM greenimpact_option
-                   WHERE id_typ = %s LIMIT 5;''', [id_typage]
-            )
-            options = cursor.fetchall()
-
-            # Construct the choices and the question dict
-            choix = [{
-                "id": option[2],
-                "nom": option[0],
-                "valeur": option[1],
-            } for option in options]
-
-            question = {
-                "titre": quest[0],
-                "categorie": categorie[0],
-                "type": nom_typage,
-                "choix": choix,
-                "unique": quest[1] == 1,
-            }
-            page_questions.append(question)
-
-    context = {
-        'questions': page_questions,
-        'page_obj': page_obj,  
-    }
+    context = {'questions': page_questions, 'page_obj': page_obj}
     return render(request, 'questions.html', context)
 
 
